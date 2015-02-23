9ff760c995177298f37b6e13782ed3bca7cc650e
Lukas <lukas.lindqvist96@gmail.com>
2015-02-23 22:12:05 +0100

    working

--- lib/Remove.py
+++ lib/Remove.py
@@ -9,9 +9,9 @@ class Remove:
         pass
 
     def book(self, book_db, book_id):
-        book = book_db.select(book_db.id(book_id))
-        file = book.title + " - " + book.author + "." + book.ext
-        os.remove('download/'+file)
+        book = book_db.get(book_db.id == book_id)
+        del_file = book.title + " - " + book.author + "." + book.ext
+        os.remove('download/'+del_file)
         book.delete_instance()
         self.test = test
 
