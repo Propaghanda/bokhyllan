9e7e7b671b1b6aa9f6f1f8825e2ff76e4b450d57
Lukas <lukas.lindqvist96@gmail.com>
2015-07-09 22:24:56 +0200

    reworked mimetype checking for .epub, now checks for mimetype file inside zip

--- lib/Mimetype.py
+++ lib/Mimetype.py
@@ -1,4 +1,6 @@
 import codecs
+import io
+import zipfile
 
 
 class Mimetype:
@@ -6,18 +8,21 @@ class Mimetype:
     def __init__(self):
         pass
 
-    def get_type(self, data):
+    def get_type(self, data): #  Checks input buffer for valid file types
         hexify = codecs.getencoder('hex')
 
-        hexdata = hexify(data[:10])[0]
-        print(hexdata)
+        hexdata = hexify(data[:5])[0]
 
-        for i in [b'504b0304140000080000', b'504b0304140016080000']:
-            if hexify(data[:10])[0] == i:
-                return 'application/epub+zip'
-            else:
-                continue
-        if hexify(data[:5])[0] == b'255044462d':
+        if hexify(data[:4])[0] == b'504b0304': # Reads the 4 first bytes to check if the file is a zip
+            iodata = io.BytesIO(data)
+            zip = zipfile.ZipFile(iodata)
+            try:
+                m = zip.read('mimetype') # Checks for the file mimetype inside the zip to make sure it's an epub
+                if m == b'application/epub+zip':
+                    return 'application/epub+zip'
+            except KeyError:
+                pass
+        if hexify(data[:5])[0] == b'255044462d': # Reads the first 5 bytes to check for the pdf signature %PDF-
             return 'application/pdf'
         else:
             return 'Unknown filetype, '+str(hexdata)
--- lib/Scan.py
+++ lib/Scan.py
@@ -4,9 +4,9 @@ from lib.Edit import Edit
 from lib.EbookLib import EbookLib
 from lib.Book import Book
 from lib.Checksum import Checksum
+from lib.Mimetype import Mimetype
 import hashlib
 import os
-import mimetypes
 
 class Scan:
     def __init(self):
@@ -14,21 +14,24 @@ class Scan:
     
     def epub(self, path):
         checksum = Checksum()
-        save = Save()
         edit = Edit()
         elib = EbookLib()
+        mime = Mimetype()
         for item in os.listdir(path):
-            mime = mimetypes.guess_type(item)
+            #mime = mimetypes.guess_type(item)
             print(item)
-            if mime[0] == 'application/epub+zip':
-                with open(path+item, "rb") as my_file:
-                    data = my_file.read()
-                    md5 = hashlib.md5(data).hexdigest()
+            with open(path+item, "rb") as my_file:
+                data = my_file.read()
+            m = mime.get_type(data)
+            if m == 'application/epub+zip':
+                md5 = hashlib.md5(data).hexdigest()
                 if checksum.check(md5):
                     elib.epub(path+item)
                     elib.res["md5"] = md5
                     edit.new(path+item, elib.res)
                     elib.epub_image(Book(edit.lastid))
                     print (elib.res)
-                print (mime)
-                print (md5)
+                    print (md5)
+                else:
+                    pass
+            print (m)
